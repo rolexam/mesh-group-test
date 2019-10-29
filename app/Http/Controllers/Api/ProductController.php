9c856dc2477043df7c5cbeb9153c4c6cb46c78a0
simonzohrabyan <simon.zohrabyan@gmail.com>
2019-10-29 20:31:18 +0300

    Product resource added

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\ProductRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Product;
+use App\Http\Resources\Product as ProductResource;
+use App\Http\Resources\ProductCollection;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new ProductCollection(Product::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $validated = $request->validated();
+
+        $product = Product::create($validated);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validated = $request->validated();
+
+        $product->fill($validated);
+        $product->save();
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return new ProductResource($product);
+    }
+}
