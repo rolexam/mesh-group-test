3a05315a912d76ba8207742de72d003fe9bc0bec
simonzohrabyan <simon.zohrabyan@gmail.com>
2019-10-29 18:26:58 +0300

    Product factoy changed

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,6 +11,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Product::class, 10)->create();
+        factory(App\Product::class, 50)->create();
     }
 }
